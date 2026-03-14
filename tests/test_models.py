6311639bf2db71e0f7dfac1b4079acca66d653d1
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2026-03-14 14:03:52 -0700

    Add test for default_targets invariant on DetailedEntry subclasses

---
+++ tests/test_models.py
@@ -0,0 +1,19 @@
+import pytest
+
+from rofi_rbw.models.card import Card
+from rofi_rbw.models.credentials import Credentials
+from rofi_rbw.models.note import Note
+
+
+@pytest.mark.parametrize(
+    "entry",
+    [
+        Note("test", "", [], "some notes"),
+        Credentials("test", "", [], "user", "pass", False, "", []),
+        Card("test", "", [], None, "1234", None, "1", "2025", None, None),
+    ],
+    ids=["note", "credentials", "card"],
+)
+def test_default_targets_return_values(entry):
+    for target in entry.default_targets:
+        assert entry[target] is not None
